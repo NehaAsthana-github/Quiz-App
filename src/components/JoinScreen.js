2dacfe92e405ebfc262a4301a7af9369012c8505
https://github.com/NehaAsthana-github <asthananeha2005@gmail.com>
2023-03-19 11:44:46 +0530

    first commit

---
+++ src/components/JoinScreen.js
@@ -0,0 +1,12 @@
+function  JoinScreen({start}){
+ return (
+<div className="join-screen">
+<h2>Join Quiz </h2>
+<p>Lorem ipsum dolor sit amet,consecteture adipsicing elit</p>
+<button onClick={start}>Star</button>
+</div>
+
+ )
+
+}
+export default JoinScreen;
